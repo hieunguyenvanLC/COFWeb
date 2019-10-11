d55f31a66c1cb710c36d16f8245cd4f7c636ff8b
Phannhathoang1996 <phannhathoang1996@gmail.com>
2019-10-11 13:33:56 +0700

    update code

---
+++ COF.AzureFunctions/Functions/ActiveCOFUserWebFunction.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace COF.AzureFunctions.Functions
+{
+    public static class ActiveCOFUserWebFunction
+    {
+        [FunctionName("ActiveCOFUserWebFunction")]
+        public async static void Run([TimerTrigger("0/50 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            HttpClient client = new HttpClient();
+            await client.GetAsync("http://cof.coffee/");
+            await client.GetAsync("http://cof.coffee/");
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
